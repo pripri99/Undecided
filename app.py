3ba3df95ff5e8127322665c29330f1490ca923f4
pripri99 <36493403+pripri99@users.noreply.github.com>
2022-02-11 22:32:57 -0500

    added config file added templates

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # App Config 
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route("/")
 def hello():
-  return "My name is Manpreet!"
+  #return "My name is Manpreet!"
+  return render_template('pages/index.html')
 
 if __name__ == "__main__":
   app.run()
