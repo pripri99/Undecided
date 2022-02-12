0259bfb5a538ed619cb7982aa6278df55568311e
pripri99 <36493403+pripri99@users.noreply.github.com>
2022-02-11 22:43:50 -0500

    modify login base and register html pages

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 @app.route("/")
-def hello():
-  #return "My name is Kerushani!"
+def index():
   return render_template('pages/index.html')
+
+@app.route("/register")
+def register():
+  return render_template('pages/auth/register.html')
+
+@app.route("/login")
+def login():
+  return render_template('pages/auth/login.html')
+
+@app.route("/logout")
+def logout():
+  return "Login out"
 
 if __name__ == "__main__":
   app.run()
